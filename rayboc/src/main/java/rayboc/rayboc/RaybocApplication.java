66c7ff6dd259afdfa81d6cd7e6e22b015478b210
Ryo Niigata <ryoniigata-0803@i.softbank.jp>
2021-08-22 13:15:47 +0900

    [add]spring boot project追加

---
+++ rayboc/src/main/java/rayboc/rayboc/RaybocApplication.java
@@ -0,0 +1,13 @@
+package rayboc.rayboc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaybocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaybocApplication.class, args);
+	}
+
+}
---
+++ rayboc/src/test/java/rayboc/rayboc/RaybocApplicationTests.java
@@ -0,0 +1,13 @@
+package rayboc.rayboc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaybocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
